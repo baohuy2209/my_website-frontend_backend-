cde735491c261f72151fb2a2bc9976fba787cc62
baohuy2209 <104624325+baohuy2209@users.noreply.github.com>
2024-07-27 13:14:43 +0700

    IP address using ReactJs

---
+++ Template_built_with_frontend/react/Form/ip_address_reactjs/src/App.js
@@ -0,0 +1,39 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Map from "./components/Map";
+
+function App() {
+  const [ipDetails, setIpDetails] = useState([]);
+  const [lat, setLat] = useState(22.5726);
+  const [lon, setLon] = useState(88.3832);
+
+  useEffect(() => {
+    axios.get("https://ipapi.co/json/").then((res) => {
+      setIpDetails(res.data);
+      setLat(res.data.latitude);
+      setLon(res.data.longitude);
+    });
+  }, []);
+  return (
+    <>
+      <h1 className="heading">IP Address Finder</h1>
+      <div className="App">
+        <div className="left">
+          <h4>What is my IPv4 address?</h4>
+          <h1 id="ip">{ipDetails.ip}</h1>
+          <h4>Approximate location: </h4>
+          <p>
+            {ipDetails.city}, {ipDetails.region}, {ipDetails.country_name}.
+          </p>
+
+          <h4>Internet Service Provider</h4>
+          <p>{ipDetails.org}</p>
+        </div>
+        <Map lat={lat} lon={lon} />
+      </div>
+    </>
+  );
+}
+
+export default App;
