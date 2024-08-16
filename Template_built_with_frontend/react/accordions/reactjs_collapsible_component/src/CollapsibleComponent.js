9485fe278e80490e0fc960a49a57f098bd94aa81
baohuy2209 <104624325+baohuy2209@users.noreply.github.com>
2024-08-16 19:44:49 +0700

    collapsible component using reactjs ###

---
+++ Template_built_with_frontend/react/accordions/reactjs_collapsible_component/src/CollapsibleComponent.js
@@ -0,0 +1,54 @@
+import React from "react";
+// import '!style-loader!css-loader!./css/style.css';
+export class CollapsibleComponent extends React.Component {
+  componentDidMount() {
+    const { name } = this.props;
+    let selector = name ? "#rcc" + name + " " : " ";
+    let acc = document.querySelectorAll(selector + ".accordion-head");
+    let i;
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function (e) {
+        e.preventDefault();
+        // Highlist the button - add/remove "active-accordion" class
+        this.classList.toggle("active-accordion");
+        let panel;
+        if (
+          this.dataset.showContentAboveButton === "true" &&
+          this.previousElementSibling !== null &&
+          this.previousElementSibling.classList.contains("panel-accordion")
+        ) {
+          panel = this.previousElementSibling;
+        } else if (
+          this.nextElementSibling !== null &&
+          this.previousElementSibling !== null &&
+          this.nextElementSibling.classList.contains("panel-accordion") &&
+          this.previousElementSibling.classList.contains("panel-accordion")
+        ) {
+          panel = this.nextElementSibling;
+        } else if (
+          this.nextElementSibling !== null &&
+          this.previousElementSibling !== null &&
+          !this.nextElementSibling.classList.contains("panel-accordion") &&
+          this.previousElementSibling.classList.contains("panel-accordion")
+        ) {
+          panel = this.previousElementSibling;
+        } else if (
+          this.nextElementSibling !== null &&
+          this.nextElementSibling.classList.contains("panel-accordion")
+        ) {
+          panel = this.nextElementSibling;
+        } else {
+          return false;
+        }
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
+  }
+  render() {
+    return <div id={"rcc" + this.props.name}>{this.props.children}</div>;
+  }
+}
---
+++ Template_built_with_frontend/react/accordions/reactjs_collapsible_component/src/CollapsibleHead.js
@@ -0,0 +1,19 @@
+import React from "react";
+export class CollapsibleHead extends React.Component {
+  render() {
+    const { className } = this.props;
+    let addClassName = className ? className : "";
+    return (
+      <button
+        data-show-content-above-button={this.props.showContentAboveButton}
+        className={
+          "accordion-head" +
+          (this.props.isExpanded ? "active-accordion" : "") +
+          addClassName
+        }
+      >
+        {this.props.children}
+      </button>
+    );
+  }
+}
